2fb080cf07d34d3de7013e775147c0fc3b711388
Divyansh Singh <singhdivyansh0890@gmail.com>
2026-01-17 22:28:10 +0530

    update in ForgotPassword component
    Commented out the import statement for 'react-toastify'.

--- pages/ForgotPassword.jsx
+++ pages/ForgotPassword.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import api from "../src/api/axios";
-import { toast } from "react-toastify";
+// import { toast } from "react-toastify";
 import { Link, useNavigate } from "react-router-dom";
 
 const ForgotPassword = () => {
